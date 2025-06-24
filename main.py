78aef09d395b65807ab8d3dafd0d154039642f27
Cleme Cajar <ccajarlaguna@gmail.com>
2025-06-24 11:43:52 -0500

    se creó la interacción del usuario

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 import openai
 import config
+
+openai.api_key = config.API_key 
+
+response = openai.ChatCompletion.create(
+    model="GPT-4o",
+    messages=[{
+        "role": "user",
+        "content": "¿cúal es el proposito de la vida?"
+    }]
+)
